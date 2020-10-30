7ed23580f3701d975bfeda88b7860a7fe2026380
maki10111 <makibyet101@gmail.com>
2020-10-30 17:48:35 +0000

    some changes

--- config.h
+++ config.h
@@ -147,7 +147,7 @@ static Key keys[] = {
 	{ MODKEY,				XK_v,		spawn,		{.v = code} },
 	{ MODKEY,				XK_e,		spawn,		SHCMD(TERMINAL " -e lf ~/") },
 	{ MODKEY,				XK_o,		spawn,		{.v = libreoffice} },
-	{ MODKEY,				XK_g,		spawn,		SHCMD("games") },
+	{ MODKEY,				XK_g,		spawn,		SHCMD(TERMINAL " -e games") },
 	{MODKEY,				XK_q,		killclient,	{0} },
 
 
