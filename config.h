e64af81835a9d7d4d447e2e81b86a50379e277ca
maki10111 <makibyet101@gmail.com>
2020-11-02 14:04:29 +0000

    added barpadding patch

--- config.h
+++ config.h
@@ -14,10 +14,12 @@ static const int swallowfloating    = 0;        /* 1 means swallow floating wind
 static const int smartgaps          = 0;        /* 1 means no outer gap when there is only one window */
 static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
-static const int user_bh            = 15;        /* 0 means that dwm will calculate bar height, >= 1 means dwm will user_bh as bar height */
+static const int vertpad            = 7;       /* vertical padding of bar */
+static const int sidepad            = 20;       /* horizontal padding of bar */
+static const int user_bh            = 20;        /* 0 means that dwm will calculate bar height, >= 1 means dwm will user_bh as bar height */
 static const int startontag         = 0;        /* 0 means no tag active on start */
-static const char *fonts[]          = { "MesloLGS NF:size=10", "JoyPixels:pixelsize=12:antialias=true:autohint=true"  };
-static char dmenufont[]             = "MesloLGS NF:size=10";
+static const char *fonts[]          = { "MesloLGS NF:size=8", "JoyPixels:pixelsize=10:antialias=true:autohint=true"  };
+static char dmenufont[]             = "MesloLGS NF:size=8";
 static char normbgcolor[]           = "#000000";
 static char normbordercolor[]       = "#00fa9a";
 static char normfgcolor[]           = "#eeeeee";
